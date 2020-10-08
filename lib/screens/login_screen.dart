b66e9978bd76aa8a353da9923e51f14daf3778bd
Wouter Bosch <wfcbosch@gmail.com>
2020-10-08 23:37:15 +0200

    removed standard code and added first route

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:animecountdown/constant.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
