9fd88431a974b533a1d0c7267af14e991b4c53bd
Wouter Bosch <wfcbosch@gmail.com>
2020-10-10 23:40:31 +0200

    fixed keyboard overflow problem

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -33,91 +33,96 @@ class _LoginScreenState extends State<LoginScreen> {
               image: themeWizard.getLoginBackground(), fit: BoxFit.cover)),
       child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 8.0),
-                child: Image(
-                  height: 300.0,
-                  image: AssetImage("assets/images/noface.png"),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 30.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        LoginMode(
-                          text: "Register",
-                          color: loginMode ? kGreyAccent : primaryColor,
-                          align: Alignment.centerRight,
-                          onTap: () {
-                            changeMode();
-                          },
-                        ),
-                        VerticalDivider(
-                          width: 75.0,
-                          thickness: 2.0,
-                          color: kGreyAccent,
-                        ),
-                        LoginMode(
-                          text: "Login",
-                          color: loginMode ? primaryColor : kGreyAccent,
-                          align: Alignment.centerLeft,
-                          onTap: () {
-                            changeMode();
-                          },
-                        ),
-                      ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 8.0),
+                  child: Image(
+                    height: 300.0,
+                    image: AssetImage("assets/images/noface.png"),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 30.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          LoginMode(
+                            text: "Register",
+                            color: loginMode ? kGreyAccent : primaryColor,
+                            align: Alignment.centerRight,
+                            onTap: () {
+                              changeMode();
+                            },
+                          ),
+                          VerticalDivider(
+                            width: 75.0,
+                            thickness: 2.0,
+                            color: kGreyAccent,
+                          ),
+                          LoginMode(
+                            text: "Login",
+                            color: loginMode ? primaryColor : kGreyAccent,
+                            align: Alignment.centerLeft,
+                            onTap: () {
+                              changeMode();
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: <Widget>[
-                        InputTextField(
-                          obscureText: false,
-                          onChanged: () {},
-                          icon: FontAwesomeIcons.user,
-                          iconColor: primaryColor,
-                          hintText: 'Username',
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        InputTextField(
-                          obscureText: true,
-                          onChanged: () {},
-                          icon: FontAwesomeIcons.lock,
-                          iconColor: primaryColor,
-                          hintText: 'Password',
-                        ),
-                      ],
+                    SizedBox(
+                      height: 20.0,
                     ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  ThinOutlineBtn(
-                      primaryColor: primaryColor,
-                      darkColor: darkColor,
-                      text: loginMode ? 'LOGIN' : 'REGISTER',
-                      ontap: () {}),
-                ],
-              ),
-            ],
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: <Widget>[
+                          InputTextField(
+                            obscureText: false,
+                            onChanged: () {},
+                            icon: FontAwesomeIcons.user,
+                            iconColor: primaryColor,
+                            hintText: 'Username',
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          InputTextField(
+                            obscureText: true,
+                            onChanged: () {},
+                            icon: FontAwesomeIcons.lock,
+                            iconColor: primaryColor,
+                            hintText: 'Password',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ThinOutlineBtn(
+                        primaryColor: primaryColor,
+                        darkColor: darkColor,
+                        text: loginMode ? 'LOGIN' : 'REGISTER',
+                        ontap: () {}),
+                  ],
+                ),
+              ],
+            ),
           )),
     );
   }
